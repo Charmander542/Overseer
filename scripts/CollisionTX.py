0df211f79612f9b29313268fd870efdfcff7d61a
Charlie Van Hook <62162108+Charmander542@users.noreply.github.com>
2025-07-28 12:30:09 -0400

    Update CollisionTX.py

--- scripts/CollisionTX.py
+++ scripts/CollisionTX.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Not titled yet
+# Title: LoRa TX with Power Control and Auto-Exit (Corrected)
 # GNU Radio version: 3.10.11.0
 
 from gnuradio import blocks
@@ -24,18 +24,19 @@ import gnuradio.lora_sdr as lora_sdr
 import threading
 
 
-# A new custom block to send a burst of N messages.
+# A new custom block to send a burst of N messages and then signal completion.
 class MessageBurstSource(gr.basic_block):
     """
     A custom GNU Radio source block that generates a specific number of
-    sequenced messages (e.g., "TEST1", "TEST2", ...) at a regular interval
-    and then stops.
+    sequenced messages (e.g., "TEST1", "TEST2", ...) at a regular interval,
+    signals completion, and then stops.
 
     Args:
         num_messages (int): The total number of messages to send.
         interval_ms (float): The time interval between messages in milliseconds.
+        done_event (threading.Event): An event to set when all messages are sent.
     """
-    def __init__(self, num_messages=10, interval_ms=2000.0):
+    def __init__(self, num_messages=10, interval_ms=2000.0, done_event=None):
         gr.basic_block.__init__(self,
             name="Message Burst Source",
             in_sig=None,
@@ -47,6 +48,8 @@ class MessageBurstSource(gr.basic_block):
         self._sent_count = 0
         self._thread = None
         self._stop_event = threading.Event()
+        # Event to signal completion to the main thread
+        self._done_event = done_event
 
     def start(self):
         """Called by the scheduler to start the block's thread."""
@@ -67,40 +70,49 @@ class MessageBurstSource(gr.basic_block):
         while not self._stop_event.is_set() and self._sent_count < self.num_messages:
             self._sent_count += 1
             msg_str = f"TEST{self._sent_count}"
+            
+            # --- THIS IS THE CORRECTED LINE ---
             msg = pmt.intern(msg_str)
+            # ----------------------------------
+
             self.message_port_pub(pmt.intern('strobe'), msg)
 
-            print(f"Sent message: {msg_str}")
+            print(f"Sent message: {msg_str} ({self._sent_count}/{self.num_messages})")
 
             # Wait for the interval, but allow for a quick exit if stop() is called.
             self._stop_event.wait(self.interval_s)
         
-        # EDIT: This code runs after the loop has finished.
-        # Check if the process wasn't stopped prematurely.
+        # This code runs after the loop has finished or been stopped.
         if not self._stop_event.is_set():
             print("Finished sending all messages.")
-            # Print the ASCII bell character to produce a "ding" sound.
-            print('\a')
+            # Signal the main thread that we are done
+            if self._done_event:
+                print("Signaling flowgraph to shut down.")
+                self._done_event.set()
 
 
 class CollisionTX(gr.top_block):
 
-    def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
-        self.flowgraph_started = threading.Event()
+    def __init__(self, power): # Accept power argument
+        gr.top_block.__init__(self, "Collision TX", catch_exceptions=True)
+        
+        # Event to signal that transmission is complete
+        self.transmission_done = threading.Event()
 
         ##################################################
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 500e3
         self.freq = freq = 910.3e6
+        # Store the power variable
+        self.power = power
 
         ##################################################
         # Blocks
         ##################################################
 
         self.uhd_usrp_sink_0 = uhd.usrp_sink(
-            ",".join(("serial=3134B8C", '')),
+            ",".join(("serial=3134BCA", '')),
             uhd.stream_args(
                 cpu_format="fc32",
                 args='',
@@ -109,11 +121,11 @@ class CollisionTX(gr.top_block):
             "",
         )
         self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        # No synchronization enforced.
-
         self.uhd_usrp_sink_0.set_center_freq(freq, 0)
         self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
-        self.uhd_usrp_sink_0.set_gain(100, 0)
+        # Set gain from the power argument
+        self.uhd_usrp_sink_0.set_gain(self.power, 0)
+
         self.lora_tx_0 = lora_sdr.lora_sdr_lora_tx(
             bw=125000,
             cr=1,
@@ -121,10 +133,17 @@ class CollisionTX(gr.top_block):
             impl_head=False,
             samp_rate=500000,
             sf=7,
-         ldro_mode=2,frame_zero_padd=1280,sync_word=[0x34] )
+            ldro_mode=2,
+            frame_zero_padd=1280,
+            sync_word=[0x34]
+        )
 
-        # EDIT: Changed num_messages from 100 to 10 to match the request.
-        self.message_burst_source_0 = MessageBurstSource(num_messages=100, interval_ms=5000)
+        # Changed num_messages to 10 and pass the completion event
+        self.message_burst_source_0 = MessageBurstSource(
+            num_messages=10, 
+            interval_ms=2000, 
+            done_event=self.transmission_done
+        )
 
 
         ##################################################
@@ -133,6 +152,9 @@ class CollisionTX(gr.top_block):
         self.msg_connect((self.message_burst_source_0, 'strobe'), (self.lora_tx_0, 'in'))
         self.connect((self.lora_tx_0, 0), (self.uhd_usrp_sink_0, 0))
 
+    # New method to allow the main thread to wait for completion
+    def wait_for_completion(self):
+        self.transmission_done.wait()
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -147,31 +169,50 @@ class CollisionTX(gr.top_block):
     def set_freq(self, freq):
         self.freq = freq
         self.uhd_usrp_sink_0.set_center_freq(self.freq, 0)
+    
+    def get_power(self):
+        return self.power
+        
+    def set_power(self, power):
+        self.power = power
+        self.uhd_usrp_sink_0.set_gain(self.power, 0)
 
 
-
-
+# Main function updated to handle arguments and automatic shutdown
 def main(top_block_cls=CollisionTX, options=None):
-    tb = top_block_cls()
+    
+    # Setup command-line argument parser
+    parser = ArgumentParser(description="Transmit a burst of LoRa messages and then exit.")
+    parser.add_argument("-p", "--power", type=int, default=90,
+                        help="Set the transmit gain for the USRP sink [0-100]. Default is 90.")
+    options = parser.parse_args()
+
+    # Create the flowgraph, passing the power argument
+    tb = top_block_cls(power=options.power)
 
     def sig_handler(sig=None, frame=None):
+        print("\n>>> SIGINT or SIGTERM detected. Shutting down cleanly.")
+        # Set the event to release the main thread if it's waiting
+        tb.transmission_done.set()
         tb.stop()
         tb.wait()
-
         sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
+    # Start the flowgraph
     tb.start()
-    tb.flowgraph_started.set()
+    print(f"Flowgraph started. Transmitting with gain: {options.power}")
 
-    try:
-        input('Press Enter to quit: ')
-    except EOFError:
-        pass
+    # Wait for the MessageBurstSource to signal that it's done
+    tb.wait_for_completion()
+
+    # Stop and wait for the flowgraph to shut down completely
+    print("Transmission complete. Stopping flowgraph...")
     tb.stop()
     tb.wait()
+    print("Flowgraph closed. Exiting.")
 
 
 if __name__ == '__main__':
