123e2550912cc892ee38234e54009503282f536e
Charlie Van Hook <62162108+Charmander542@users.noreply.github.com>
2025-07-30 14:38:16 -0400

    Update run_tx_arduino.sh

--- bash_path/run_tx_arduino.sh
+++ bash_path/run_tx_arduino.sh
@@ -6,11 +6,11 @@ BAUD=115200
 
 # === Ensure serial port exists ===
 if [ ! -e "$PORT" ]; then
-  echo "[catsniffer] Error: Serial port $PORT does not exist."
+  echo "[arduino] Error: Serial port $PORT does not exist."
   exit 1
 fi
 
-echo "[catsniffer] Listening on $PORT at ${BAUD} baud..."
+echo "[arduino] Listening on $PORT at ${BAUD} baud..."
 
 # === Configure port ===
 stty -F "$PORT" cs8 "$BAUD" igncr -ixon -icanon -echo
